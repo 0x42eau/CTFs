c50b4a36391562c25d76d40f4a28ce4b0cc9e407
0x42eau <49952735+0x42eau@users.noreply.github.com>
2024-06-16 19:27:27 -0600

    Create nmap_to_version.sh

---
+++ nmap_to_version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ./nmap_to_version.sh <ip>
+
+if [ $# -ne 5 ]; then
+	echo 'Usage: ./nmap_to_version.sh <ip>'
+	echo 'Example: ./nmap_to_version.sh 10.10.11.12'
+	exit -1
+fi
+
+ip=$1
+
+nmap -p- -vv -Pn -n $ip -oA all-ports
+
+cat all-ports.xml | grep -i portid | awk '{print $3}' | awk -F '"' '{print $2}' > ports.txt
+
+nmap -p$(cat ports.txt) -sV -sC -Pn -n -vv $ip -oA service-scan
