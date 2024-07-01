8cf21afd8beac55a2fdcabdb4e1da9b4d3184687
0x42eau <49952735+0x42eau@users.noreply.github.com>
2024-06-30 18:30:12 -0600

    Update nmap_to_version.sh

--- nmap_to_version.sh
+++ nmap_to_version.sh
@@ -15,3 +15,18 @@ nmap -p- -vv -Pn -n $ip -oA all-ports
 cat all-ports.xml | grep -i portid | awk '{print $3}' | awk -F '"' '{print $2}' > ports.txt
 
 nmap -p$(paste -sd, - < ports.txt) -sV -sC -Pn -n -vv $ip -oA service-scan
+
+echo ''
+echo ''
+echo '############################'
+echo 'running udp scan - can take some time'
+echo '############################'
+echo ''
+echo ''
+sleep 3s
+
+
+nmap -sU --top-ports 100 -Pn -n -vv $ip -oA udp-top100
+cat udp-top100.xml | grep -i portid | awk '{print $3}' | awk -F '"' '{print $2}' > udpports.txt
+nmap -p$(paste -sd, - < udpports.txt) -sU -sV -sC -Pn -n -vv $ip -oA udpservice-scan
+
